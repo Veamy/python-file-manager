13eb227318b77daa0d34cdb0fffaa8952d642d9f
Veamy <134639266+Veamy@users.noreply.github.com>
2023-05-28 11:13:33 +0300

    Update os_com.py

--- file_dvij/os_com.py
+++ file_dvij/os_com.py
@@ -3,20 +3,25 @@ import os
 # Додатковий файл для роботи з файлами і директоріями
 import shutil
 
+
 # Повертає список ЛОКАЛЬНИХ файлів
 def list_files():
     files = os.listdir('.')
     for file in files:
-        green_text = "\033[32m{}\033[0m".format(file)
-        print(green_text)
+        print(f"\033[32m{file}\033[0m")
 
 # Створюємо папку
 def create_directory(directory_name):
+    if directory_name == None:
+        directory_name = input("\033[94mВведіть ім'я папки: \033[0m")
     os.mkdir(directory_name)
     print(f"Створена папка '{directory_name}'")
 
 # Перейменовуємо файл
 def rename_file(old_name, new_name):
+    if new_name == None:
+        old_name = input("\033[94mВведіть поточне ім'я файлу: \033[0m")
+        new_name = input("\033[94mВведіть нове ім'я файлу: \033[0m")
     try:
         os.rename(old_name, new_name)
         print(f"Файл '{old_name}' перейменовано в '{new_name}'")
@@ -26,11 +31,15 @@ def rename_file(old_name, new_name):
 # !!! ДЛЯ ВИДАЛЕННЯ ПАПКИ І ФАЙЛІВ ВИКОРИСТОВУЮТЬСЯ РІЗНІ КОМАНДИ !!!
 # Видаляємо папку (регістрозалежно)
 def delete_directory(directory_name):
+    if directory_name == None:
+        directory_name = input("\033[94mВведіть ім'я папки: \033[0m ")
     shutil.rmtree(directory_name)
     print(f"Видалена папка '{directory_name}'")
 
 # Видаляємо файл (регістрозалежно)
 def delete_file(file_name):
+    if file_name == None:
+        file_name = input("\033[94mВведіть ім'я файлу: \033[0m")
     try:
         os.remove(file_name)
         print(f"Видалений файл '{file_name}'")
@@ -39,6 +48,8 @@ def delete_file(file_name):
 
 # Для переміщення між папками
 def change_directory(directory_path):
+    if directory_path == None:
+        directory_path = input('\033[94mВведіть ім\'я/шлях папки: \033[0m')
     try:
         os.chdir(directory_path)
     except FileNotFoundError:
@@ -46,6 +57,9 @@ def change_directory(directory_path):
 
 # Відкриваємо файл
 def open_file(file_name):
+    if file_name == None:
+        file_name = input("\033[94mВведіть ім'я файлу:\033[0m ")
+    print("\n\n\n")
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
             content = file.read()
@@ -58,6 +72,17 @@ def open_file(file_name):
         print(f"Немає доступу до файлу '{file_name}'.")
     except Exception as e:
         print(f"Сталася помилка: {e}")
+
+
+#створення файлу 
+def touch(path):
+    if path == None :
+        path = input("\033[94mВведіть ім'я для файлу:\033[0m ")
+    try:
+        with open(path, 'a'):
+            os.utime(path, None)
+    except OSError as e:
+        print(f"Сталася помилка при створенні файлу: {e}")
 
 
 
